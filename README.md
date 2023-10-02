7ac17dd748051e9927b534b8697a6834b0184eff
cupid-dust <ahsanmansoor7572@gmail.com>
2023-10-02 11:26:06 +0500

    env added

--- README.md
+++ README.md
@@ -49,3 +49,4 @@ You should have a `.env.local` with the following:
 - AWS_ACCESS_KEY_ID
 - AWS_SECRET_ACCESS_KEY
 - AWS_BUCKET_NAME
+- AWS_REGION_NAME
--- utils/s3.ts
+++ utils/s3.ts
@@ -2,7 +2,7 @@ import aws from 'aws-sdk';
 import { randomUUID } from 'crypto';
 
 const s3 = new aws.S3({
-  region: 'eu-west-1',
+  region: process.env.AWS_REGION_NAME as string,
   accessKeyId: process.env.AWS_ACCESS_KEY_ID as string,
   secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY as string,
 });
